1c460be70ace4b3ecf105195218327acb613e034
niganma888 <131949914+niganma888@users.noreply.github.com>
2025-05-23 00:05:19 +0800

    Update fgsm.py

--- Self-writtenCode/shenqi/fgsm.py
+++ Self-writtenCode/shenqi/fgsm.py
@@ -6,11 +6,10 @@ from torchvision import datasets, transforms
 import numpy as np
 import matplotlib.pyplot as plt
 
-
-
+# Hyperparameters
 epsilons = [0, .05, .1, .15, .2, .25, .3]
-pretrained_model = 'lenet_mnist_model.pth'   # 这个是模型参数文件读取路径，改为自己存放该文件的目录
-use_cuda=True
+pretrained_model = 'lenet_mnist_model.pth'  # Path to the pretrained model file, update to your directory
+use_cuda = True  # Use GPU if available
 
 # LeNet Model definition
 class Net(nn.Module):
@@ -33,13 +32,12 @@ class Net(nn.Module):
 
 # MNIST Test dataset and dataloader declaration
 test_loader = torch.utils.data.DataLoader(
-    datasets.MNIST('../data', train=False, download=True, transform=transforms.Compose([
-            transforms.ToTensor(),
-            ])),
-        batch_size=1, shuffle=True)
+    datasets.MNIST('../data', train=False, download=True, transform=transforms.Compose([transforms.ToTensor()])),
+    batch_size=1, shuffle=True
+)
 
 # Define what device we are using
-print("CUDA Available: ",torch.cuda.is_available())
+print("CUDA Available: ", torch.cuda.is_available())
 device = torch.device("cuda" if (use_cuda and torch.cuda.is_available()) else "cpu")
 
 # Initialize the network
@@ -48,80 +46,53 @@ model = Net().to(device)
 # Load the pretrained model
 model.load_state_dict(torch.load(pretrained_model, map_location=device))
 
-# Set the model in evaluation mode. In this case this is for the Dropout layers
+# Set the model in evaluation mode (important for Dropout layers)
 model.eval()
 
 # FGSM attack code
 def fgsm_attack(image, epsilon, data_grad):
-    # Collect the element-wise sign of the data gradient
     sign_data_grad = data_grad.sign()
-    # Create the perturbed image by adjusting each pixel of the input image
-    perturbed_image = image + epsilon*sign_data_grad
-    # Adding clipping to maintain [0,1] range
-    perturbed_image = torch.clamp(perturbed_image, 0, 1)
-    # Return the perturbed image
+    perturbed_image = image + epsilon * sign_data_grad
+    perturbed_image = torch.clamp(perturbed_image, 0, 1)  # Keep image in valid range
     return perturbed_image
 
-def test( model, device, test_loader, epsilon ):
-
-    # Accuracy counter
+# Test function for evaluating the model with FGSM attack
+def test(model, device, test_loader, epsilon):
     correct = 0
     adv_examples = []
 
-    # Loop over all examples in test set
     for data, target in test_loader:
-
-        # Send the data and label to the device
         data, target = data.to(device), target.to(device)
+        data.requires_grad = True  # Enable gradient calculation
 
-        # Set requires_grad attribute of tensor. Important for Attack
-        data.requires_grad = True
-
-        # Forward pass the data through the model
         output = model(data)
-        init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
+        init_pred = output.max(1, keepdim=True)[1]
 
-        # If the initial prediction is wrong, dont bother attacking, just move on
         if init_pred.item() != target.item():
             continue
 
-        # Calculate the loss
         loss = F.nll_loss(output, target)
-
-        # Zero all existing gradients
         model.zero_grad()
-
-        # Calculate gradients of model in backward pass
         loss.backward()
 
-        # Collect datagrad
         data_grad = data.grad
-
-        # Call FGSM Attack
         perturbed_data = fgsm_attack(data, epsilon, data_grad)
 
-        # Re-classify the perturbed image
         output = model(perturbed_data)
+        final_pred = output.max(1, keepdim=True)[1]
 
-        # Check for success
-        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
         if final_pred.item() == target.item():
             correct += 1
-            # Special case for saving 0 epsilon examples
             if (epsilon == 0) and (len(adv_examples) < 5):
                 adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+                adv_examples.append((init_pred.item(), final_pred.item(), adv_ex))
         else:
-            # Save some adv examples for visualization later
             if len(adv_examples) < 5:
                 adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
+                adv_examples.append((init_pred.item(), final_pred.item(), adv_ex))
 
-    # Calculate final accuracy for this epsilon
-    final_acc = correct/float(len(test_loader))
-    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
-
-    # Return the accuracy and an adversarial example
+    final_acc = correct / float(len(test_loader))
+    print(f"Epsilon: {epsilon}\tTest Accuracy = {correct} / {len(test_loader)} = {final_acc}")
     return final_acc, adv_examples
 
 accuracies = []
@@ -133,9 +104,8 @@ for eps in epsilons:
     accuracies.append(acc)
     examples.append(ex)
 
-print(examples)
-
-plt.figure(figsize=(5,5))
+# Plot Accuracy vs Epsilon
+plt.figure(figsize=(5, 5))
 plt.plot(epsilons, accuracies, "*-")
 plt.yticks(np.arange(0, 1.1, step=0.1))
 plt.xticks(np.arange(0, .35, step=0.05))
@@ -143,188 +113,25 @@ plt.title("Accuracy vs Epsilon")
 plt.xlabel("Epsilon")
 plt.ylabel("Accuracy")
 plt.show()
-filename = 'attack_effect.png'  # 这个是量fgsm算法结果图保存路径，运行前换为自己的路径（提前建好文件夹）
-plt.savefig( filename )
+
+# Save the plot
+filename = 'attack_effect.png'  # Path to save the plot, update with your directory
+plt.savefig(filename)
 plt.close()
 
 # Plot several examples of adversarial samples at each epsilon
 cnt = 0
-plt.figure(figsize=(8,10))
+plt.figure(figsize=(8, 10))
 for i in range(len(epsilons)):
     for j in range(len(examples[i])):
         cnt += 1
-        plt.subplot(len(epsilons),len(examples[0]),cnt)
+        plt.subplot(len(epsilons), len(examples[0]), cnt)
         plt.xticks([], [])
         plt.yticks([], [])
         if j == 0:
-            plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=14)
-        orig,adv,ex = examples[i][j]
-        plt.title("{} -> {}".format(orig, adv))
-        plt.imshow(ex, cmap="gray")
-plt.tight_layout()
-plt.show()
-
-# def pgd_attack(model, images, labels, epsilon, iters=4) :
-#     images = images.to(device)
-#     labels = labels.to(device)
-#     loss = nn.CrossEntropyLoss()
-#     ori_images = images.data
-#     alpha=epsilon / 4
-
-#     for i in range(iters) :
-#         outputs = model(images)
-
-#         model.zero_grad()
-#         cost = loss(outputs, labels).to(device)
-#         cost.backward()
-#         adv_images = images + alpha*images.grad.sign()
-
-#         eta = torch.clamp(adv_images - ori_images, min=-epsilon, max=epsilon)
-#         images = torch.clamp(ori_images + eta, min=0, max=1).detach_()
-
-#     return images
-
-def pgd_attack(model, image, label, epsilon, iters=4) :
-    ori_image = image.data
-    alpha = epsilon / 4
-
-
-    for i in range(iters) :
-        image.requires_grad = True
-        output = model(image)
-        loss = F.nll_loss(output, label).to(device)
-        model.zero_grad()
-        loss.backward()
-        grad = image.grad
-        adv_images = image + alpha*grad.sign()
-        eta = torch.clamp(adv_images - ori_image, min=-epsilon, max=epsilon)
-        image = torch.clamp(ori_image + eta, min=0, max=1).detach_()
-
-    return image
-
-# # Send the data and label to the device
-#         data, target = data.to(device), target.to(device)
-
-#         # Set requires_grad attribute of tensor. Important for Attack
-#         data.requires_grad = True
-
-#         # Forward pass the data through the model
-#         output = model(data)
-#         init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-
-#         # If the initial prediction is wrong, dont bother attacking, just move on
-#         if init_pred.item() != target.item():
-#             continue
-
-#         # Calculate the loss
-#         loss = F.nll_loss(output, target)
-
-#         # Zero all existing gradients
-#         model.zero_grad()
-
-#         # Calculate gradients of model in backward pass
-#         loss.backward()
-
-#         # Collect datagrad
-#         data_grad = data.grad
-
-# # FGSM attack code
-# def fgsm_attack(image, epsilon, data_grad):
-#     # Collect the element-wise sign of the data gradient
-#     sign_data_grad = data_grad.sign()
-#     # Create the perturbed image by adjusting each pixel of the input image
-#     perturbed_image = image + epsilon*sign_data_grad
-#     # Adding clipping to maintain [0,1] range
-#     perturbed_image = torch.clamp(perturbed_image, 0, 1)
-#     # Return the perturbed image
-#     return perturbed_image
-
-def test_pgd( model, device, test_loader, epsilon):
-
-    # Accuracy counter
-    correct = 0
-    adv_examples = []
-
-    # Loop over all examples in test set
-    for data, target in test_loader:
-
-        # Send the data and label to the device
-        data, target = data.to(device), target.to(device)
-
-        # Set requires_grad attribute of tensor. Important for Attack
-        data.requires_grad = True
-
-        # Forward pass the data through the model
-        output = model(data)
-        init_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-
-        # If the initial prediction is wrong, dont bother attacking, just move on
-        if init_pred.item() != target.item():
-            continue
-
-        # Call PGD Attack
-        perturbed_data = pgd_attack(model, data, target, epsilon)
-
-        # Re-classify the perturbed image
-        output = model(perturbed_data)
-
-        # Check for success
-        final_pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-        if final_pred.item() == target.item():
-            correct += 1
-            # Special case for saving 0 epsilon examples
-            if (epsilon == 0) and (len(adv_examples) < 5):
-                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
-        else:
-            # Save some adv examples for visualization later
-            if len(adv_examples) < 5:
-                adv_ex = perturbed_data.squeeze().detach().cpu().numpy()
-                adv_examples.append( (init_pred.item(), final_pred.item(), adv_ex) )
-
-    # Calculate final accuracy for this epsilon
-    final_acc = correct/float(len(test_loader))
-    print("Epsilon: {}\tTest Accuracy = {} / {} = {}".format(epsilon, correct, len(test_loader), final_acc))
-
-    # Return the accuracy and an adversarial example
-    return final_acc, adv_examples
-
-accuracies_pgd = []
-examples_pgd = []
-
-# Run test for each epsilon
-for eps in epsilons:
-    acc, ex = test_pgd(model, device, test_loader, eps)
-    accuracies_pgd.append(acc)
-    examples_pgd.append(ex)
-
-plt.figure(figsize=(5,5))
-plt.plot(epsilons, accuracies, 'g--', label='FGSM')
-plt.plot(epsilons, accuracies_pgd, 'r-', label='PGD')
-plt.yticks(np.arange(0, 1.1, step=0.1))
-plt.xticks(np.arange(0, .35, step=0.05))
-plt.title("Accuracy vs Epsilon")
-plt.xlabel("Epsilon")
-plt.ylabel("Accuracy")
-plt.legend()
-filename = 'pdg_attack_effect.png'  # 这个是量算法对比结果图保存路径，运行前换为自己的路径（提前建好文件夹）
-plt.savefig( filename )
-plt.show()
-plt.close()
-
-# Plot several examples of adversarial samples at each epsilon
-cnt = 0
-plt.figure(figsize=(8,10))
-for i in range(len(epsilons)):
-    for j in range(len(examples_pgd[i])):
-        cnt += 1
-        plt.subplot(len(epsilons),len(examples_pgd[0]),cnt)
-        plt.xticks([], [])
-        plt.yticks([], [])
-        if j == 0:
-            plt.ylabel("Eps: {}".format(epsilons[i]), fontsize=14)
-        orig,adv,ex = examples_pgd[i][j]
-        plt.title("{} -> {}".format(orig, adv))
+            plt.ylabel(f"Eps: {epsilons[i]}", fontsize=14)
+        orig, adv, ex = examples[i][j]
+        plt.title(f"{orig} -> {adv}")
         plt.imshow(ex, cmap="gray")
 plt.tight_layout()
 plt.show()
